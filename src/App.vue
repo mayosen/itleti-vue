a4cb4a459bf566e040b9d3f985e0948efc066973
mayosen <gare577@gmail.com>
2023-03-19 12:19:18 +0300

    Close self-closing tag explicitly
    Use simple toLowerCase() function

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <script setup>
   import { ref } from "vue";
-  import css from "./App.module.css"
+  import css from "./App.module.css";
 
   const cards = ref([
-      {name: "Пачка молока", price: "70"},
-      {name: "Пачка сока", price: "100"},
-      {name: "Десяток яиц", price: "70"},
-      {name: "Огурцы колючие", price: "12"},
-      {name: "Огурцы гладкие", price: "10"},
+    {name: "Пачка молока", price: "70"},
+    {name: "Пачка сока", price: "100"},
+    {name: "Десяток яиц", price: "70"},
+    {name: "Огурцы колючие", price: "12"},
+    {name: "Огурцы гладкие", price: "10"},
   ]);
   const nameFilter = ref("");
 </script>
 
 <template>
   <label for="nameFilter">Введите фильтр: </label>
-  <input id="nameFilter" type="text" v-model="nameFilter">
+  <input id="nameFilter" type="text" v-model="nameFilter" />
 
   <div>
     <template v-for="card in cards" :key="card.name">
-      <div :class="css.card" v-if="card.name.toLocaleLowerCase().includes(nameFilter.toLocaleLowerCase())">
+      <div :class="css.card" v-if="card.name.toLowerCase().includes(nameFilter.toLowerCase())">
         <div>{{ card.name }}</div>
         <div :class="css.price">{{ card.price }}</div>
       </div>
