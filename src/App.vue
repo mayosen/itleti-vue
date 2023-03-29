66ed8cc1825c0f2655b64e633635135aaf0df366
mayosen <gare577@gmail.com>
2023-03-29 18:31:59 +0300

    Add modal window

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <script setup>
-  import ProductList from "./components/Product/ProductList.vue";
+  import Modal from "./components/Lessons/Modal.vue";
+  import { ref } from "vue";
+
+  const showModal = ref(false);
 </script>
 
 <template>
-  <ProductList></ProductList>
+  <button @click="showModal = !showModal">Показать</button>
+  <Modal :show="showModal" @close="showModal = false">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam labore non nostrum omnis quae quas quasi quibusdam
+    voluptate voluptates! A accusamus ad aliquam aspernatur aut corporis delectus ducimus ea esse ex exercitationem fuga
+    incidunt nostrum, odio optio porro provident quam reprehenderit rerum sit veritatis voluptas voluptatum. Atque
+    consequuntur dolores obcaecati omnis provident unde vel voluptas. Accusantium alias, animi architecto assumenda
+    atque blanditiis dolore incidunt ipsam laboriosam laborum libero maiores, minima odio perferendis quaerat rem sed,
+    similique veritatis vitae voluptatem. Aliquid aut corporis culpa dolorum explicabo fugiat laudantium nesciunt
+    perspiciatis qui reprehenderit. Adipisci assumenda consequuntur deserunt dolore dolorem explicabo numquam, optio.
+  </Modal>
 </template>
 
-<style scoped>
+<style>
+  body {
+    font-family: Arial, serif;
+  }
 </style>
---
+++ src/components/Lessons/Modal.vue
@@ -0,0 +1,69 @@
+<script setup>
+  import { Teleport } from "vue";
+  import Button from "./Button.vue";
+
+  defineEmits(["close"]);
+  defineProps(["show"]);
+</script>
+
+<template>
+  <Teleport to="#modal">
+    <Transition>
+      <div class="modal__overlay" v-if="show" @click.self="$emit('close')">
+        <div class="modal">
+          <div class="modal__header">
+            <div>Модальное окно</div>
+            <button @click="$emit('close')">&times;</button>
+          </div>
+          <div class="modal__body">
+            <slot></slot>
+          </div>
+        </div>
+      </div>
+    </Transition>
+  </Teleport>
+</template>
+
+<style scoped>
+  .modal {
+    width: clamp(300px, 50%, 600px);
+    border-radius: 10px;
+    padding: 15px 10px;
+    background: white;
+    margin: 0 20px;
+  }
+
+  .modal__header {
+    display: flex;
+    align-items: center;
+    padding-bottom: 10px;
+  }
+
+  .modal__header div {
+    flex: 1 1 auto;
+  }
+
+  .modal__header button {
+    border: none;
+    background: none;
+    cursor: pointer;
+    font-size: 1.2rem;
+  }
+
+  .modal__overlay {
+    position: absolute;
+    inset: 0 0 0 0;
+    background: rgba(0, 0, 0, 0.7);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .v-enter-from, .v-leave-to {
+    opacity: 0;
+  }
+
+  .v-enter-active, .v-leave-active {
+    transition: opacity 400ms ease;
+  }
+</style>
